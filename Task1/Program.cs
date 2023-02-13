60c2ea076863eac3e73069a104b22230e6f972e5
Polina <ms.maylin@inbox.ru>
2023-02-13 23:18:58 +0300

    Task1 added

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+void NaturalNumbersOutput(int boundary)
+{
+    if (boundary == 1) Console.Write(boundary + " ");
+    else
+    {
+        NaturalNumbersOutput(boundary - 1);
+        Console.Write(boundary + " ");
+    }
+}
+
+Random rand = new Random();
+int intervalBoundary = rand.Next(1, 101);
+NaturalNumbersOutput(intervalBoundary);
